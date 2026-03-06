899b6d022932d630a990f99f38e965f76cfa7ee3
Gustavo Fao Valvassori <faogustavo@gmail.com>
2026-03-06 14:55:05 -0300

    big bang

---
+++ README.md
@@ -0,0 +1,7 @@
+# KJWT
+
+Kotlin Multiplatform implementation of the JWT protocol using:
+
+- Kotlinx.Coroutines - https://github.com/Kotlin/kotlinx.coroutines
+- Kotlinx.Serialization - https://github.com/Kotlin/kotlinx.serialization
+- Cryptography Kotlin - https://github.com/whyoleg/cryptography-kotlin
---
+++ lib/build.gradle.kts
@@ -0,0 +1,25 @@
+import org.jetbrains.kotlin.gradle.dsl.JvmTarget
+
+plugins {
+    alias(libs.plugins.kotlin.multiplatform)
+    alias(libs.plugins.kotlin.serialization)
+}
+
+kotlin {
+    jvm()
+    iosX64()
+    iosArm64()
+    iosSimulatorArm64()
+
+    sourceSets {
+        commonMain.dependencies {
+            implementation(libs.kotlinx.coroutines.core)
+            implementation(libs.kotlinx.serialization.json)
+            implementation(libs.cryptography.core)
+        }
+        commonTest.dependencies {
+            implementation(libs.kotlin.test)
+            implementation(libs.cryptography.providerOptimal)
+        }
+    }
+}
