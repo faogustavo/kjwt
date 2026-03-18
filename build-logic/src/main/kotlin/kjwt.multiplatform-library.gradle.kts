718deefec9324a586105cb9b832c80af57d6966c
Gustavo Fao Valvassori <faogustavo@gmail.com>
2026-03-18 16:39:03 -0300

    downgrading language version to support kotlin 2.1

--- build-logic/src/main/kotlin/kjwt.multiplatform-library.gradle.kts
+++ build-logic/src/main/kotlin/kjwt.multiplatform-library.gradle.kts
@@ -15,4 +15,15 @@ kotlin {
     allTargets()
     configureTests()
     explicitApi()
+
+    compilerOptions {
+        languageVersion.set(org.jetbrains.kotlin.gradle.dsl.KotlinVersion.KOTLIN_2_2)
+        apiVersion.set(org.jetbrains.kotlin.gradle.dsl.KotlinVersion.KOTLIN_2_2)
+    }
+
+    sourceSets {
+        all {
+            languageSettings.optIn("kotlin.time.ExperimentalTime")
+        }
+    }
 }
